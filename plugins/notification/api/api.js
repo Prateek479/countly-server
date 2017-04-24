3e144b0b361f7c824bbb0702e48a0445161bb186
prateek479 <prateek479@gmail.com>
2017-04-25 02:26:11 +0530

    adding pulgin configuration

--- plugins/notification/api/api.js
+++ plugins/notification/api/api.js
@@ -4,10 +4,65 @@ var common = require('../../../api/utils/common.js'),
 
 
 (function (plugins) {
-	//write api cal
+
+	//Storing data in array of json which can further be stored in mongo collection 
+	//write api for real tim enotifications.
+	const coditions = [{
+		'name' :'user session count reach x',
+		'field' : 'app_user',
+		'value' : 8,
+		'path' : 'sc',
+		'notification_msg' : 'You just got luck on your x visit..!'
+	},{
+		'name' :'notify if platform is web',
+		'field' : 'app',
+		'value' : 'web',
+		'path' : 'type',
+		'notification_msg': 'Coupen Code "COUNTLY" available on web'
+	},{
+		'name' :'Evenet base notification',
+		'field' : 'events',
+		'value' : 1,
+		'path' : 'sum',
+		// if true check the segments condition
+		'isSegment' : false, 
+		'segment' : {
+			// there could be multiple condition based on segment comes under this
+			'field':'', 
+			'value':'',
+			'path' : ''
+		}, 
+		'notification_msg': 'Coupen Code "COUNTLY" available on web'
+	}];
+
 	plugins.register("/i", function(ob){
-		console.log('here is my object',ob)
+		const param =  ob.params	
+		console.log('params', param)
+		coditions.forEach(function(cond) {
+
+			// not for custom event base notification
+			if(cond.field !== 'events'){
+				if(param && param[cond.field] && param[cond.field][cond.path] === cond.value) {
+					console.log('notify user ' , cond.notification_msg);				
+				}
+			}	
+			//custom event notification
+			else if(cond.field === 'events') {
+				console.log('meet ',param.qstring[cond.field])
+				if(!cond.isSegment && param.qstring && param.qstring[cond.field] && param.qstring[cond.field][cond.path] === cond.value){
+					console.log('notify user for this event', cond.notification_msg);
+				} else if(cond.isSegment && param.qstring && param.qstring[cond.segment.field] && param.qstring[cond.segment.field][cond.segment.path] === cond.segment.value){
+					console.log('notify user for segment event', cond.notification_msg);
+				}
+			}
+		});
 	});
+
+	// notificaiton system for dail ybase notifications
+	plugins.register("/master", function(ob){
+    	//regestering the jobs 
+    });
+
 }(plugins));
 
 module.exports = plugins;
